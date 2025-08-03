ee1dbb87108228ca07a9c74be7327a4abae5375c
Tanji_Nishi <116243015+Sinhanishita@users.noreply.github.com>
2025-08-03 16:36:18 +0600

    Add files via upload

---
+++ countingdivisor1.cpp
@@ -0,0 +1,37 @@
+/// IN THE NAME OF ALLAH
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define slow ios_base::sync_with_stdio(false); cin.tie(NULL)
+#define nl '\n'
+#define ll long long
+#define sp ' '
+#define PI 3.14159265358979323846
+#define __lcm(a, b) ((a / __gcd(a, b)) * b)
+#define db double
+#define pb push_back
+#define f first
+#define s second
+#define all(x) x.begin(), x.end()
+#define allr(x) x.rbegin(), x.rend()
+#define cn continue
+#define mod 1000000007
+
+const int N = 1e6 + 5;
+int d[N];
+
+
+int32_t main() {
+    slow;
+    for(int i = 1; i < N; i++){
+        for(int j = i; j < N; j+=i){
+            d[j]++;
+        }
+    }
+    int n; cin >> n;
+    while(n--){
+        int x; cin >> x;
+        cout << d[x] << nl;
+    }
+}
